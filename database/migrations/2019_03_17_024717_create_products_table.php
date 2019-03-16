d2ca3d89d1bfc91bdd1153330919da39a8587ff1
ahmed.iqu <jniqbal1@gmail.com>
2019-03-17 02:52:15 +0600

    Product Table added

---
+++ database/migrations/2019_03_17_024717_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('category_id');
+            $table->string('title',128)->unique();
+            $table->string('slug',128)->unique();
+            $table->longText('description');
+            $table->tinyInteger('in_stoke')->defult(1);
+            $table->decimal('price',8,2);
+            $table->decimal('sale_price',8,2)->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
