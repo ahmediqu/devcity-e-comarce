c803e9ef7b4cc058a00a3dcfb9210ee61067c83b
ahmed.iqu <jniqbal1@gmail.com>
2019-03-17 02:46:00 +0600

    Categories table added

---
+++ database/migrations/2019_03_17_024309_create_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name',128)->unique();
+            $table->string('slug',128)->unique();
+            $table->string('banner',128);
+            $table->unsignedInteger('category_id')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
